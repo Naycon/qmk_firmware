648d96967af1eab6af743f9fb93d8b93911a83ee
Johan Persson <johanopersson@gmail.com>
2021-06-18 22:46:14 +0200

    Update coppertyl default layout to 5x3 layout.

--- keyboards/coppertyl/rev1/rev1.h
+++ keyboards/coppertyl/rev1/rev1.h
@@ -25,20 +25,19 @@
  * The second converts the arguments into a two-dimensional array which
  * represents the switch matrix.
  */
-// TODO
 #define LAYOUT( \
-    L00, L01, L02, L03, L04, L05,                     R06, R07, R08, R09, R10, R11, \
-    L12, L13, L14, L15, L16, L17,                     R18, R19, R20, R21, R22, R23, \
-    L24, L25, L26, L27, L28, L29, L30, L31, R32, R33, R34, R35, R36, R37, R38, R39, \
-                   L40, L41, L42, L43, L44, R45, R46, R47, R48, R49 \
-) \
-{ \
-    { KC_NO, KC_NO, L05,   L04,   L03,   L02,   L01,   L00   }, \
-    { KC_NO, KC_NO, L17,   L16,   L15,   L14,   L13,   L12   }, \
-    { L31,   L30,   L29,   L28,   L27,   L26,   L25,   L24   }, \
-    { L44,   L43,   L42,   L41,   L40,   KC_NO, KC_NO, KC_NO }, \
-    { KC_NO, KC_NO, R06,   R07,   R08,   R09,   R10,   R11   }, \
-    { KC_NO, KC_NO, R18,   R19,   R20,   R21,   R22,   R23   }, \
-    { R32,   R33,   R34,   R35,   R36,   R37,   R38,   R39   }, \
-    { R45,   R46,   R47,   R48,   R49,   KC_NO, KC_NO, KC_NO }, \
-}
+  L00, L01, L02, L03, L04,           R00, R01, R02, R03, R04, \
+  L10, L11, L12, L13, L14,           R10, R11, R12, R13, R14, \
+  L20, L21, L22, L23, L24,           R20, R21, R22, R23, R24, \
+                 L30, L31, L32, R30, R31, R32 \
+  ) \
+  { \
+    { KC_NO, L00, L01, L02, L03, L04 }, \
+    { KC_NO, L10, L11, L12, L13, L14 }, \
+    { KC_NO, L20, L21, L22, L23, L24 }, \
+    { KC_NO, KC_NO, KC_NO, L30, L31, L32 }, \
+    { KC_NO, R04, R03, R02, R01, R00 }, \
+    { KC_NO, R14, R13, R12, R11, R10 }, \
+    { KC_NO, R24, R23, R22, R21, R20 }, \
+    { KC_NO, KC_NO, KC_NO, R32, R31, R30 } \
+  }
